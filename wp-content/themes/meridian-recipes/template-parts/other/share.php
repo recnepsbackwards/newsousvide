1a8dd0fc379c4a673fa46d60c0f7112de56a2811
Spencer West <spencer.west@kbb.com>
2018-08-09 13:53:07 -0700

    updates

--- wp-content/themes/meridian-recipes/template-parts/other/share.php
+++
@@ -1,6 +0,0 @@
-<div class="share-post-overlay">
-	<div class="share-post">
-		<div class="share-post-inner"><div class="text-align-center"><span class="fa fa-refresh fa-spin"></span></div></div>
-		<span class="share-post-close">X</span>
-	</div><!-- .share-post -->
-</div><!-- .share-post-overlay -->
